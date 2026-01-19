76a198bf99900e7a3989e47b0601fc910ff74307
Conner685 <cponton@my.bcit.ca>
2026-01-18 17:20:08 -0800

    Initial Commit of file structure and CSS file

---
+++ lang/messages/en/user.js
@@ -0,0 +1,9 @@
+export const TEXT = {
+    writerTitle: "Note Writer",
+    readerTitle: "Note Reader",
+    lastStored: "Stored at: ",
+    addBtn: "Add Note",
+    removeBtn: "Remove",
+    backBtn: "Back to Index",
+    placeholder: "Type your note here..."
+};
